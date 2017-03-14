03fdebb5739e65c14ea12deaa9f2eef62ba2b14f
Trygve Lie <post@trygve-lie.com>
2017-03-14 17:01:49 +0100

    added middelware to provide path for the bundle(s)

--- examples/simple.js
+++ examples/simple.js
@@ -1,19 +1,31 @@
 'use strict';
 
 const express = require('express');
+const Assets = require('../');
 const app = express();
 
-const Assets = require('../');
+// set up asset bundler
 const assets = new Assets('./assets/es5/main.js');
 
+// listen for events
 assets.on('update', (ids) => {
     ids.forEach((id) => {
         console.log('updated:', id);
     });
 });
 
+// provide bundle info to each request
+app.use(assets.middelware());
+
+// attach bundle routes
 app.use(assets.router());
 
+// print where bundle is
+app.get('/', (req, res, next) => {
+    res.status(200).send(`JS bunde is at: ${res.locals.js}`);
+})
+
+// start server
 const server = app.listen(8080, () => {
     console.log(`http server - Bundle at: http://localhost:${server.address().port}/js/`);
 });
